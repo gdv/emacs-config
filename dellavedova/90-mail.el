35124529917541c584bfbb820156afada32cf76d
Gianluca Della Vedova <gianluca@dellavedova.org>
2011-09-29 13:09:13 +0200

    from post-mode to mail-mode

--- dellavedova/90-mail.el
+++ dellavedova/90-mail.el
@@ -1,18 +1,14 @@
-(autoload 'post-mode "post" "mode for e-mail" t)
 (add-to-list 'auto-mode-alist
-             '("\\.*mutt-*\\|.article\\|\\.followup"
-               . post-mode))
-
-(add-hook 'post-mode-hook
-  (lambda()
-    (auto-fill-mode t)
-    (setq fill-column 72)    ; rfc 1855 for usenet messages
-    (setq make-backup-files nil) ;;; No backups necessary.
-    (require 'boxquote)))
+             '("\\mutt-*\\|.article\\|\\.followup"
+               . mail-mode))
 
 (add-hook
    'mail-mode-hook
    (lambda ()
+     (auto-fill-mode t)
+     (setq fill-column 72)    ; rfc 1855 for usenet messages
+     (setq make-backup-files nil) ;;; No backups necessary.
+     (require 'boxquote)
      (define-key mail-mode-map [(control c) (control c)]
        (lambda ()
          (interactive)
